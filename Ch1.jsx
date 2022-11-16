a5420ecbcea80b027fc6056e05e6fb6d01421ed6
David Mendoza <hectormza21@gmail.com>
2022-11-16 16:45:47 -0400

    initial commit

---
+++ Ch1.jsx
@@ -0,0 +1,13 @@
+const Ch1 = () => {
+  return (
+    <div className="min-h-screen grid place-content-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="bg-emerald-500 w-52 h-52 rounded-full shadow-2xl grid place-content-center">
+        <div className="bg-teal-200 w-32 h-32 rounded-full grid place-content-center">
+          <div className="bg-red-500 w-16 h-16 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ch1;
