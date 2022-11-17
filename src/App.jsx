086eae1094c7d0c1320a2e449a7bd4dda992f6c5
David Mendoza <hectormza21@gmail.com>
2022-11-17 15:29:34 -0400

    Contact section css html. Wrapper bug fix.

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Rockets from "./components/Rockets";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
       <Header />
-      <Hero />
-      <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
-      <Rockets />
+      <main className="mx-auto max-w-4xl ">
+        <Hero />
+        <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
+        <Rockets />
 
-      <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
-      <Testimonials />
+        <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
+        <Testimonials />
 
-      <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
-
-      <section id="contact" className="my-12 p-6">
-        <h2 className="mb-6 text-center text-4xl font-bold text-slate-900 dark:text-white sm:text-5xl">
-          Contact Us
-        </h2>
-      </section>
+        <hr className="mx-auto w-1/2 bg-black  dark:bg-white" />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
