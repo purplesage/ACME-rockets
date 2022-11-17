4698d0c68579b319026c8af07972894d2047a339
David Mendoza <hectormza21@gmail.com>
2022-11-17 15:02:12 -0400

    testimonials section html css

---
+++ src/components/Rockets.jsx
@@ -0,0 +1,65 @@
+import rocketMan from "../assets/rocketman.png";
+import rocketRide from "../assets/rocketride.png";
+import rocketLaunch from "../assets/rocketlaunch.png";
+
+const Rockets = () => {
+  return (
+    <section id="rockets" className="my-12 mb-6 scroll-mt-20 p-6 ">
+      <h2 className="text-center text-4xl font-bold text-slate-900 dark:text-white sm:text-5xl">
+        Our Rockets
+      </h2>
+
+      <ul className="m-12 mx-auto flex list-none flex-col items-center gap-8 sm:flex-row">
+        <li className="flex w-2/3 flex-col items-center rounded-3xl border-solid border-slate-900 bg-white py-6 px-2 shadow-xl dark:border-gray-100 dark:bg-black sm:w-5/6 ">
+          <img src={rocketMan} alt="Explorer" className="mb-6 w-1/2" />
+
+          <h3 className="text-center text-3xl text-slate-900 dark:text-white">
+            Explorer
+          </h3>
+
+          <p className="mt-2 hidden text-center text-3xl text-slate-500 dark:text-slate-400 sm:block">
+            $
+          </p>
+
+          <p className="mt-2 text-center text-2xl text-slate-500 dark:text-slate-400 sm:hidden">
+            Affordable Exploration
+          </p>
+        </li>
+
+        <li className="flex w-2/3 flex-col items-center rounded-3xl border-solid border-slate-900 bg-white py-6 px-2 shadow-xl dark:border-gray-100 dark:bg-black sm:w-5/6 ">
+          <img src={rocketRide} alt="adventurer" className="mb-6 w-1/2" />
+
+          <h3 className="text-center text-3xl text-slate-900 dark:text-white">
+            Adventurer
+          </h3>
+
+          <p className="mt-2 hidden text-center text-3xl text-slate-500 dark:text-slate-400 sm:block">
+            $$
+          </p>
+
+          <p className="mt-2 text-center text-2xl text-slate-500 dark:text-slate-400 sm:hidden">
+            Best Selling Rocket!
+          </p>
+        </li>
+
+        <li className="flex w-2/3 flex-col items-center rounded-3xl border-solid border-slate-900 bg-white py-6 px-2 shadow-xl dark:border-gray-100 dark:bg-black sm:w-5/6 ">
+          <img src={rocketLaunch} alt="Infinity" className="mb-6 w-1/2" />
+
+          <h3 className="text-center text-3xl text-slate-900 dark:text-white">
+            Infinity
+          </h3>
+
+          <p className="mt-2 hidden text-center text-3xl text-slate-500 dark:text-slate-400 sm:block">
+            $$$
+          </p>
+
+          <p className="mt-2 text-center text-2xl text-slate-500 dark:text-slate-400 sm:hidden">
+            Luxury Starship
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Rockets;
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,62 @@
+import { BsDashLg } from "react-icons/bs";
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials" className="my-12 p-6">
+      <h2 className="mb-6 text-center text-4xl font-bold text-slate-900 dark:text-white sm:text-5xl">
+        Testimonials
+      </h2>
+      <figure className="my-12">
+        <blockquote className="relative rounded-3xl bg-teal-600 py-12 pl-14 pr-8 dark:bg-black">
+          <p className="mt-2 text-left text-2xl text-white before:absolute before:top-0 before:left-0 before:translate-x-2 before:translate-y-2 before:transform before:font-serif before:text-8xl before:text-white before:opacity-25 before:content-['\201C'] after:absolute after:bottom-0 after:right-3 after:translate-x-2 after:translate-y-2 after:transform after:font-serif after:text-8xl after:text-white after:opacity-25 after:content-['\201D'] dark:text-slate-400 sm:text-3xl">
+            ACME has always been there for me. Their Explorer rocket arrived in
+            a wooden crate as expected. Life-long customer! A++ shopping
+            experience.
+          </p>
+        </blockquote>
+        <figcaption className="mt-4 text-right text-xl italic text-slate-500 dark:text-slate-400 sm:text-2xl">
+          <p className=" flex items-center justify-end">
+            <BsDashLg /> Wile E. Coyote, Genius
+          </p>
+        </figcaption>
+      </figure>
+
+      <figure className="my-12">
+        <blockquote className="relative rounded-3xl bg-teal-600 py-12 pl-14 pr-8 dark:bg-black">
+          <p className="mt-2 text-left text-2xl text-white before:absolute before:top-0 before:left-0 before:translate-x-2 before:translate-y-2 before:transform before:font-serif before:text-8xl before:text-white before:opacity-25 before:content-['\201C'] after:absolute after:bottom-0 after:right-3 after:translate-x-2 after:translate-y-2 after:transform after:font-serif after:text-8xl after:text-white after:opacity-25 after:content-['\201D'] dark:text-slate-400 sm:text-3xl">
+            The ACME Adventurer has thwarted my Illudium Q-36 Explosive Space
+            Modulator on several occasions.{" "}
+            <span className="italic">This makes me very, very angry!</span>{" "}
+            Nevertheless, K-9 and i have awarded ACME the Martian contract for
+            space exploration rockets based on ACME's quality and sturdy
+            designs.
+          </p>
+        </blockquote>
+        <figcaption className="mt-4 text-right text-xl italic text-slate-500 dark:text-slate-400 sm:text-2xl">
+          <p className=" flex items-center justify-end">
+            <BsDashLg /> Marvin The Martian &amp; K-9
+          </p>
+        </figcaption>
+      </figure>
+
+      <figure className="my-12">
+        <blockquote className="relative rounded-3xl bg-teal-600 py-12 pl-14 pr-8 dark:bg-black">
+          <p className="mt-2 text-left text-2xl text-white before:absolute before:top-0 before:left-0 before:translate-x-2 before:translate-y-2 before:transform before:font-serif before:text-8xl before:text-white before:opacity-25 before:content-['\201C'] after:absolute after:bottom-0 after:right-3 after:translate-x-2 after:translate-y-2 after:transform after:font-serif after:text-8xl after:text-white after:opacity-25 after:content-['\201D'] dark:text-slate-400 sm:text-3xl">
+            I knew i not only wanted &#8212;
+            <span className="italic">i needed</span> &#8212; ACME's Infinity
+            Rocket for my mission in space. Acme delivered in one day! Nothing
+            says <q className="italic">Take me to your leader</q> like ACME's
+            infinity Rocket!
+          </p>
+        </blockquote>
+        <figcaption className="mt-4 text-right text-xl italic text-slate-500 dark:text-slate-400 sm:text-2xl">
+          <p className=" flex items-center justify-end">
+            <BsDashLg /> Buzz Lightyear
+          </p>
+        </figcaption>
+      </figure>
+    </section>
+  );
+};
+
+export default Testimonials;
